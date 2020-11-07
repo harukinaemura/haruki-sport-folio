8b053c41aedbfb274524accd6f59d80e6848a82d
harukinaemura <73884682+harukinaemura@users.noreply.github.com>
2020-11-07 19:43:29 +0900

    Add files via upload

---
+++ js/my_script.js
@@ -0,0 +1,19 @@
+//TOPボタン
+$(function(){
+
+    $('.page-top').hide();  
+  
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {    
+          $('.page-top').fadeIn();
+        } else {
+          $('.page-top').fadeOut();
+        }
+    });
+  
+    $('.page-top').click(function () {      
+        $('body,html').animate({scrollTop: 0}, 500);  
+        return false;
+    });
+  });
+  
